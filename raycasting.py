262feeaba87fb7ae7b430356c85c90b7ef76ac82
Gerrit Kocherscheidt <kochersg@koco-group.com>
2023-12-24 11:57:25 +0100

    Raycasting refactored

--- raycasting.py
+++ raycasting.py
@@ -10,40 +10,13 @@ class RayCasting:
         ox, oy = self.game.player.pos
         x_map, y_map = self.game.player.map_pos
         ray_angle = self.game.player.angle - settings.HALF_FOV + 0.0001
-        for ray in range (settings.NUM_RAYS):
+        for _ in range (settings.NUM_RAYS):
             sin_a = math.sin(ray_angle)
             cos_a = math.cos(ray_angle)
-
             # horizontals
-            y_hor, dy = (y_map+1, 1) if sin_a>0 else (y_map-1E-6, -1)
-            depth_hor = (y_hor-oy)/sin_a
-            x_hor = ox + depth_hor * cos_a
-            delta_depth = dy / sin_a
-            dx = delta_depth * cos_a
-            for i in range(settings.MAX_DEPTH):
-                tile_hor = int(x_hor), int(y_hor)
-                if tile_hor in self.game.map.world_map:
-                    break
-                x_hor += dx
-                y_hor += dy
-                depth_hor += delta_depth
-
-
-
+            depth_hor = self.ray_cast_horizontal(ox, oy, y_map, sin_a, cos_a)
             # verticals
-            x_vert, dx = (x_map+1,1) if cos_a>0 else (x_map-1E-6, -1)
-            depth_vert = (x_vert - ox)/cos_a
-            y_vert = oy + depth_vert * sin_a
-            delta_depth = dx/cos_a
-            dy = delta_depth*sin_a
-            for i in range(settings.MAX_DEPTH):
-                tile_vert = int(x_vert), int(y_vert)
-                if tile_vert in self.game.map.world_map:
-                    break
-                x_vert += dx
-                y_vert += dy
-                depth_vert += delta_depth
-
+            depth_vert = self.ray_cast_vertical(ox, oy,x_map, sin_a, cos_a)
             depth = min(depth_vert, depth_hor)
             
             pg.draw.line(
@@ -55,6 +28,32 @@ class RayCasting:
             )
 
             ray_angle += settings.DELTA_ANGLE
+
+    def calc_depth(self, x:float, y:float, dx: float, dy:float, depth: float, delta_depth:float) -> float:
+        for _ in range(settings.MAX_DEPTH):
+            tile_vert = int(x), int(y)
+            if tile_vert in self.game.map.world_map:
+                break
+            x += dx
+            y += dy
+            depth += delta_depth
+        return depth
+
+    def ray_cast_vertical(self, ox:int, oy:int, x_map: int, sin_a: float, cos_a: float)->float:
+        x_vert, dx = (x_map+1,1) if cos_a>0 else (x_map-1E-6, -1)
+        depth_vert = (x_vert - ox)/cos_a
+        y_vert = oy + depth_vert * sin_a
+        delta_depth = dx/cos_a
+        dy = delta_depth*sin_a
+        return self.calc_depth(x_vert, y_vert, dx, dy, depth_vert, delta_depth)
+
+    def ray_cast_horizontal(self, ox: float, oy: float, y_map:int, sin_a: float, cos_a: float)->float:
+        y_hor, dy = (y_map+1, 1) if sin_a>0 else (y_map-1E-6, -1)
+        depth_hor = (y_hor-oy)/sin_a
+        x_hor = ox + depth_hor * cos_a
+        delta_depth = dy / sin_a
+        dx = delta_depth * cos_a
+        return self.calc_depth(x_hor, y_hor, dx, dy, depth_hor, delta_depth)
 
 
     def update(self):
